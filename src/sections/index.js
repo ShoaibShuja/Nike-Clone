18b7a471589de41305a8ec9f3007901a25ffd680
ShoaibShuja <shoaibshuja829@gmail.com>
2024-09-01 08:04:15 +0430

    first commit

---
+++ src/sections/index.js
@@ -0,0 +1,19 @@
+import Hero from './Hero';
+import PopularProducts from './PopularProducts';
+import SuperQuality from './SuperQuality';
+import CustomerReviews from './CustomerReviews';
+import Services from './Services';
+import Subscribe from './Subscribe';
+import SpecialOffer from './SpecialOffer';
+import Footer from './Footer';
+
+export {
+  Hero,
+  PopularProducts,
+  SuperQuality,
+  CustomerReviews,
+  Services,
+  Subscribe,
+  SpecialOffer,
+  Footer
+}
